4d28aef5b7a99b093c8522c6da712aadb31ea6b4
Arthuro Verissimo <arthurobdfv@gmail.com>
2021-02-23 18:27:24 +0100

    Home and Profile Component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src="https://github.com/Arthurobdfv.png" alt="Arthuro Verissimo"/>
+            <div>
+                <strong>Arthuro Verissimo</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
