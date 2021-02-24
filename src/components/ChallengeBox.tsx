66bcf2caa09232d323c06c97f6cb231396e53952
Arthuro Verissimo <arthurobdfv@gmail.com>
2021-02-24 21:34:20 +0100

    Front-end Funcionality

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {activeChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe {activeChallenge.amount} xp</header>
+
+          <main>
+            <img src={`icons/${activeChallenge.type}.svg`} />
+            <strong>Novo Desafio</strong>
+            <p>{activeChallenge.description}</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+            <button
+              type="button"
+              onClick={resetChallenge}
+              className={styles.challengeFailedButton}
+            >
+              Falhei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber desafios</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
