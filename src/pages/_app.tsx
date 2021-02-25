b42c5ec40cba798a60a6a81031143a0a4e3b1a9d
Arthuro Verissimo <arthurobdfv@gmail.com>
2021-02-25 22:10:35 +0100

    Added Google Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,21 @@ import "../styles/global.css";
 
 import { ChallengesProvider } from "../contexts/ChallengesContext";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { Header } from "../components/Header";
+import React from "react";
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
+  const { session } = pageProps;
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    <div>
+      <Provider options={{ site: process.env.SITE }} session={session}>
+        <Header />
+        <ChallengesProvider>
+          <Component {...pageProps} />
+        </ChallengesProvider>
+      </Provider>
+    </div>
   );
 }
 
