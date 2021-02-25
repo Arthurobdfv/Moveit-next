a5d4ca3a400ab1999b76062f494e5511a2c13137
Arthuro Verissimo <arthurobdfv@gmail.com>
2021-02-25 23:33:20 +0100

    g

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,9 @@ import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   const { session } = pageProps;
+  console.log(process.env.SITE)
+  console.log(process.env.GOOGLE_ID)
+  console.log(process.env.GOOGLE_SECRET)
   return (
     <div>
       <Provider options={{ site: process.env.SITE }} session={session}>
